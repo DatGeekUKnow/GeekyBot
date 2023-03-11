96fcc22fa16f5b4b4f99a9e4e2d5eae18fe962db
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-03-10 21:36:39 -0500

    restructure and add glob package

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
