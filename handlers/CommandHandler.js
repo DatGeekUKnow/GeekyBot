83cdd398987ac1be5f42da4b92ffdae53a6bbe34
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-03-11 21:47:58 -0500

    restructure commands directory

--- handlers/CommandHandler.js
+++ handlers/CommandHandler.js
@@ -1,16 +1,16 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const Util = require('../modules/Util');
 
 module.exports = function loadEvents(bot) {
 
     // load in all command files
 
     const commandsPath = path.join(__dirname.substring(0, __dirname.lastIndexOf('/')), 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandFiles = Util.getFileList(commandsPath);
     
     for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+        const command = require(file);
         // Set a new item in the Collection with the key as the command name and the value as the exported module
         if ('data' in command && 'execute' in command) {
             bot.commands.set(command.data.name, command);
---
+++ modules/Util.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const getFileList = (dirName) => {
+    let files = [];
+    const items = fs.readdirSync(dirName, { withFileTypes: true });
+
+    for (const item of items) {
+        if (item.isDirectory()) {
+            files = [...files, ...getFileList(`${dirName}/${item.name}`)];
+        } else {
+            files.push(`${dirName}/${item.name}`);
+        }
+    }
+    return files;
+};
+
+module.exports = {
+    getFileList
+}
