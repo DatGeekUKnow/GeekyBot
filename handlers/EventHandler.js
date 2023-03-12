c06d1976ed5cc9af203575924d0930e6fe512f96
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-03-11 21:35:17 -0500

    separate eventHandler

---
+++ handlers/EventHandler.js
@@ -0,0 +1,20 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+
+module.exports = function loadEvents(bot) {
+    
+    // load in all event files.
+    const eventsPath = path.join(__dirname.substring(0, __dirname.lastIndexOf('/')), 'events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        if (event.once) {
+            bot.once(event.name, (...args) => event.execute(...args));
+        } else {
+            bot.on(event.name, (...args) => event.execute(...args));
+        }
+    }
+}
