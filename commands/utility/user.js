aca0eaa9e8a7e4fb491006e79b0286490074e579
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-10-09 21:52:02 -0400

    test ownership acknowledgement

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder }= require('discord.js');
+const { ownerId } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,6 +8,8 @@ module.exports = {
     async execute(interaction) {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
+        if (interaction.user.id == ownerId)  await interaction.reply({ content: `This command was run by the bot owner!, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
+
         await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
     },
 };
