73cf1050f7a4bbaf319cc12ec7847a140c6d61d4
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-10-10 02:32:14 +0000

    add isOwner command

---
+++ commands/utility/isOwner.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder }= require('discord.js');
+const { ownerId } = require('../../config.json');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('isowner')
+    .setDescription('Replies whether or not the sender is the bot owner'),
+    async execute(interaction) {
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        if (interaction.user.id == ownerId) await interaction.reply({ content: `Yes! You are the server owner Geek!`, ephemeral: true });
+        else await interaction.reply({ content: `Haha! You wish you created me ;)`, ephemeral: true });
+    },
+};
