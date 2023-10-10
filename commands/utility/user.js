990521fbf6c5eba8bff5c698170a265b1da0e16a
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2023-10-10 19:50:49 -0400

    revert something dumb

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder }= require('discord.js');
-const { ownerId } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,7 +7,6 @@ module.exports = {
     async execute(interaction) {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
-        if (interaction.user.id == ownerId)  await interaction.reply({ content: `This command was run by the bot owner!, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
-        else await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
+        await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
     },
 };
