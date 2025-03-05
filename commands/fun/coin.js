322d54ec47322d205ae2e3ff189c7505ef4ae8b5
Geek <girjakeandallawesomepplz@gmail.com>
2025-03-04 22:30:58 -0500

    fix attatchment path

--- commands/fun/coin.js
+++ commands/fun/coin.js
@@ -1,4 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
+const path = require('path');
+
+// path to the .gif
+const attatchmentPath = path.join(__dirname, '..', '..', 'attachments')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,7 +16,7 @@ module.exports = {
         async execute(interaction) {
             let message = interaction.options.getString('message');
             let heads = Math.random() < 0.5;
-            if (heads) await interaction.reply({ content:message, files: ['./attachments/coin_flip_heads.gif']});
-            else await interaction.reply({ content:message, files: ['./attachments/coin_flip_tails.gif']});
+            if (heads) await interaction.reply({ content:message, files: [`${attatchmentPath}/coin_flip_heads.gif`]});
+            else await interaction.reply({ content:message, files: [`${attatchmentPath}/coin_flip_tails.gif`]});
         },
 };
